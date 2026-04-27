2c4eca2b78b4d1c033f91b50ff1d5a38f3fecca5
directsgg <jorgenoeg@gmail.com>
2026-04-27 13:15:31 -0600

    Update project metadata and implement CPU module
    - Updated project title, author, description, clock frequency, and tile size in info.yaml.
    - Added cpu.v file with CPU module implementation.
    - Modified project.v to instantiate the new CPU module and updated input/output assignments.
    - Updated Makefile to include cpu.v in project sources.
    - Adjusted testbench (tb.v) to reference the new module name.
    - Updated test.py to reflect changes in input/output expectations.

---
+++ src/cpu.v
@@ -0,0 +1,164 @@
+module cpu (
+    input clk,
+    input rst,
+    input [7:0] data_in,
+    input clr_data_in,
+    input dsp_ar,
+    input set_ar,
+    input r_mem,
+    input w_mem,
+    input start_cpu,
+    input stop_cpu,
+    output [7:0] data_out,
+    output run_cpu
+);
+
+    reg S;
+    reg [2:0] SC = 0;
+    localparam 
+        T0 = 3'd0, 
+        T1 = 3'd1, 
+        T2 = 3'd2, 
+        T3 = 3'd3, 
+        T4 = 3'd4, 
+        T5 = 3'd5,
+        T6 = 3'd6; 
+
+    reg [4:0] PC;
+    reg [4:0] AR;
+    reg [7:0] IR;
+    reg [7:0] DR;
+    reg [7:0] AC;
+    reg [7:0] INPR;
+    reg [7:0] OUTR;
+
+    wire [7:0] D;
+
+    reg [7:0] mem [32:0];
+
+
+    always @(posedge clk or posedge rst) begin
+
+        if (rst) begin
+            SC <= 0;
+            PC <= 0;
+            OUTR <= 0;
+            AR <= 0;
+            S <= 0;
+            INPR <= 0;
+            OUTR <= 0;
+        end else begin
+            S <= (start_cpu & ~S) | (~stop_cpu & S);
+
+            if (S == 0) begin 
+
+                if (clr_data_in) begin
+                    INPR <= 0;
+                    OUTR <= 0;
+                end else if (data_in) begin
+                    INPR <= data_in | INPR;
+                    OUTR <= data_in | OUTR;
+                end
+
+                else if (dsp_ar) begin
+                    OUTR <= AR;
+                end
+                else if (set_ar) begin
+                    AR <= INPR[4:0];
+                end
+
+                else if (w_mem) begin
+                    mem[AR] <= INPR;
+                end
+                else if (r_mem) begin
+                    OUTR <= mem[AR];
+                end
+
+            end else begin
+                if ( (D[7] && SC == T3) ||
+                    ((D[0] || D[1] || D[2] || D[5]) && SC == T5) ||
+                    ((D[3] || D[4]) && SC == T4) ||
+                    (D[6] && SC == T6)
+                    ) begin
+                    SC <= 0;
+                end else SC <= SC + 1;
+            
+                case (SC)
+                    T0: begin
+                        AR <= PC;
+                    end
+                    T1: begin
+                        IR <= mem[AR];
+                        PC <= PC + 1;
+                    end
+                    T2: begin
+                        AR <= IR[4:0];
+                    end
+                    T3: begin
+                        if (D[7]) begin
+                            if (IR[0]) begin
+                                AC <= ~AC;
+                            end
+                            if (IR[1]) begin
+                                AC <= AC + 1;
+                            end
+                            if (IR[2]) begin
+                                S <= 0;
+                            end
+                            if (IR[3] && AC == 0) begin
+                                PC <= PC + 1;
+                            end
+                            if (IR[4]) begin
+                                OUTR <= AC;
+                            end
+                        end
+                    end
+                    T4: begin
+                        if (D[0] || D[1] || D[2] || D[5] || D[6]) begin
+                            DR <= mem[AR];
+                        end
+                        if (D[3]) begin
+                            mem[AR] <= AC;
+                        end
+                        if (D[4]) begin
+                            PC <= AR;
+                        end
+                        
+                    end
+                    T5: begin
+                        if (D[0]) begin
+                            AC <= AC & DR;
+                        end
+                        if (D[1]) begin
+                            AC <= AC + DR;
+                        end
+                        if (D[2]) begin
+                            AC <= DR;
+                        end
+                        if (D[5]) begin
+                            AC <= AC - DR;
+                        end
+                        if (D[6]) begin
+                            DR <= DR + 1;
+                        end
+                    end
+                    T6: begin
+                        if (D[6]) begin
+                            mem[AR] <= DR;
+                            if (DR == 0) begin
+                                PC <= PC + 1;
+                            end
+                        end
+                    end
+                endcase
+            end
+        end
+    end
+    
+    // Decodificador de instruccion de IR
+    assign D = 1 << IR[7:5];
+
+    assign data_out = OUTR;
+    assign run_cpu = S;
+
+endmodule
--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Jorge Gutierrez
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_directsgg_cpu8bit (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,10 +16,45 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  wire [7:0] input_data;
+
+  wire rst, clear, dsp_ar, set_ar, r_mem, w_mem, run_cpu, start_cpu, stop_cpu;
+
+  assign input_data = ui_in;
+  assign clear = uio_in[0];
+  assign dsp_ar = uio_in[1];
+  assign set_ar = uio_in[2];
+  assign r_mem = uio_in[3];
+  assign w_mem = uio_in[4];
+  assign start_cpu = uio_in[5];
+  assign stop_cpu = uio_in[6];
+  assign run_cpu = uio_out[7];
+  assign rst = rst_n;
+
+cpu my_cpu (
+        .clk(clk),
+        .rst(rst),
+        .data_in(input_data),
+        .clr_data_in(clear),
+        .dsp_ar(dsp_ar),
+        .set_ar(set_ar),
+        .w_mem(w_mem),
+        .r_mem(r_mem),
+        .start_cpu(start_cpu),
+        .stop_cpu(stop_cpu),
+        .data_out(uo_out),
+        .run_cpu(run_cpu)
+    );
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out[0] = 0;
+  assign uio_out[1] = 0;
+  assign uio_out[2] = 0;
+  assign uio_out[3] = 0;
+  assign uio_out[4] = 0;
+  assign uio_out[5] = 0;
+  assign uio_out[6] = 0;
+  assign uio_oe  = 8'b00000001;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
